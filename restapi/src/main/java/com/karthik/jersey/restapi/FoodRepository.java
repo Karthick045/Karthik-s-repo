38b0e78d500cae77bbe5d815aa40c0497ef73b70
Karthik.R <Karthick045@gmail.com>
2018-08-18 11:27:13 +0530

    repo creation commit

---
+++ restapi/src/main/java/com/karthik/jersey/restapi/FoodRepository.java
@@ -0,0 +1,38 @@
+package com.karthik.jersey.restapi;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FoodRepository {
+	
+	List<Food> foodList;
+	public FoodRepository() {
+		foodList= new ArrayList<>();
+		Food f = new Food();
+		f.setFoodCode(100);
+		f.setName("Pizza");
+		f.setPrice(430);
+		
+		Food f1 = new Food();
+		f1.setFoodCode(101);
+		f1.setName("Burger");
+		f1.setPrice(200);
+		foodList.add(f);
+		foodList.add(f1);
+	}
+	
+	public List<Food> getFoods(){
+		return foodList;
+	}
+	
+	public Food getFood(int fCode) {
+		
+		for(Food f:foodList) {
+			if(f.getFoodCode()==fCode) {
+				return f;
+			}
+		}
+		return new Food();
+	}
+
+}
--- restapi/src/main/java/com/karthik/jersey/restapi/FoodResource.java
+++ restapi/src/main/java/com/karthik/jersey/restapi/FoodResource.java
@@ -1,6 +1,5 @@
 package com.karthik.jersey.restapi;
 
-import java.util.Arrays;
 import java.util.List;
 
 import javax.ws.rs.GET;
@@ -13,17 +12,7 @@ public class FoodResource {
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public List<Food> getFood() {
-		System.out.println("Food details");
-		Food f = new Food();
-		f.setFoodCode(100);
-		f.setName("Pizza");
-		f.setPrice(430);
-		
-		Food f1 = new Food();
-		f1.setFoodCode(101);
-		f1.setName("Burger");
-		f1.setPrice(200);
-		List<Food> foodList = Arrays.asList(f,f1);
-		return foodList;
+		FoodRepository repo= new FoodRepository();
+		return repo.getFoods();
 	}
 }
